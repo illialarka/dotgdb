52d9994dcf3b120b139ff3f8c45d0934fc91ce12
Illia Larka <larkailya@gmail.com>
2022-11-03 21:12:23 +0100

    Implement begining of arguments parser

--- src/sdb/main.py
+++ src/sdb/main.py
@@ -1,69 +1,31 @@
-from subprocess import Popen, PIPE, STDOUT
-import io
-import agent
-import threading
-import socket
+import argparse
 
-# in general, I need to start debugger agent process
-# and communicate with it somehow
-
+# entry point for application which will read from stdin and write to stdout
+# parsing commands and sending them to debugger in proer format
+# 1. consume commands from stdin
+# 1.1. parse commands
+# 1.2 if command is run, start debugger process
+# 2. send command to debugger
+# 3. read response from debugger
+# 4. send response to stdout
+# 5. repeat 1-4
 def main():
-    port = find_port()
-    print("port: ", port)
+    welcome_message = """Welcome to SDB debugger.
+Type 'help' to get list of available commands.
+Type 'quit' to exit.
+"""
+    argument_parser = argparse.ArgumentParser(
+            prog = "SDB",
+            description = "Mono Soft Debugger agent")
 
-    command = [f'mono --debug --debugger-agent=transport=dt_socket,server=y,address=127.0.0.1:{port} /Users/illialarka/projects/DebuggableProgram/bin/Debug/net6.0/DebuggableProgram.dll']
-    print ("command: ", command)
+    argument_parser.add_argument("executable")
+    argument_parser.add_argument("-v", "--verbose", action="store_true", help="enable verbose logging level")
+    argument_parser.add_argument("-p", "--port", help= "port to connect in case of attaching")
 
-    p = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+    args = argument_parser.parse_args()
 
-    while True:
-        output = p.stdout.readline()
-        if output == '' and p.poll() is not None:
-            break
-        if output:
-            print (output.strip())
-    rc = p.poll()
-
-
-#    out, err = p.communicate()
-#    print (out)
-
-
-    #thread = threading.Thread(target=print_output, args=(p.stdout)).start()
-
-    # run command and listend to output in a separate thread
-    # and then send commands to debugger agent  to control the process
-
-    print ("after wait")
-
-    #agent_instance = agent.Agent()
-    #agent_instance.start(True, port, 10)
-
-    #types = agent_instance.vm.get_types()
-
-    #for type in agent_instance.vm.get_types():
-    #    print ("Type: {0}".format(type.get_fullname()))
-
-def run_command(command):
-    process = Popen(command, stdout=PIPE)
-def print_output(process):
-    out, _ = process.communicate()
-    if out is bytes:
-        print(out.decode('utf-8'))
-        return
-    else:
-        print ("print_output")
-        for line in out:
-            line = line.rstrip()
-            print (line)
-
-def find_port(port=8000):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        if s.connect_ex(("localhost", port)) == 0:
-            return find_port(port=port + 1)
-        else:
-            return port
-
+    print (args)
 
 if __name__ == "__main__":
     main()
+
