febae93ae0c5de68ee78369ecae3194e13512dbe
Illia Larka <larkailya@gmail.com>
2022-11-07 21:20:30 +0100

    Add support of simplified flow

--- src/sdb/main.py
+++ src/sdb/main.py
@@ -16,26 +16,28 @@ def main():
 
     argument_parser.add_argument("executable")
     argument_parser.add_argument("-v", "--verbose", action="store_true", help="enable verbose logging level")
+    argument_parser.add_argument("-s", "--simplified", action="store_true", help="enables simplified flow")
     argument_parser.add_argument("-p", "--port", help= "port to connect in case of attaching")
     argument_parser.add_argument("-a", "--address", help="address to connect in case of attaching", default="127.0.0.1")
 
     arguments = argument_parser.parse_args()
-    utils.configure_logger(arguments.verbose)
+    utils.configure_logger(arguments.verbose, arguments.simplified)
 
     dbg_session = session.DbgSession()
     dbg_agent = agent.DbgAgent()
 
-    # run debugger agent and debugger server
     try:
         dbg_session.run(arguments)
         dbg_agent.start(True, dbg_session.port, 10)
-    except Exception as error:
-        print ("An error occured during starting debugger session. Error:  {0}".format(error))
-        logger.error(error)
+    except exceptions.ExecutableNotFound:
+        logger.info("Couldn't find an executable to run. Ensure it exists in {arguments.executable}.")
+
+    # do not display prefix if it is simplified mode
+    tool_prefix = "sdb> " if not arguments.simplified else "";
 
     try:
         while True:
-            input_command = input("sdb> ").split(" ")
+            input_command = input(tool_prefix).split(" ")
 
             command_alias = None
             command_arguments = None
@@ -48,8 +50,6 @@ def main():
                 command_alias = input_command[0]
                 command_arguments = input_command[1:]
 
-            print (f"command: {command_alias}, arguments: {command_arguments}")
-
             command = selector.select_command(command_alias)
 
             if command is None:
@@ -57,13 +57,10 @@ def main():
                 continue
 
             print (command.execute(dbg_agent, command_arguments))
-
     except exceptions.ExitException:
         logger.info("Exit requested. Closing session and kill processes.")
         dbg_session.exit()
         dbg_agent.stop()
-    except exceptions.ExecutableNotFound:
-        logger.info("Couldn't find an executable to run. Ensure it exists in {arguments.executable}.")
     except Exception as unhandled_exception:
         logger.error(unhandled_exception)
         logger.info("Closing all session on exception.")
