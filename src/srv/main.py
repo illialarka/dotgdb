f8e045726a79dcbfa419aa8a718ca3169bf7f138
Illia Larka <larkailya@gmail.com>
2022-11-09 20:36:13 +0100

    Allow all CORS origin for HTTP request

--- src/srv/main.py
+++ src/srv/main.py
@@ -1,31 +1,30 @@
 from flask import Flask,request
+from flask_cors import cross_origin 
 from flask_socketio import SocketIO
 import argparse
 
 app = Flask(__name__)
+# TODO: make cors allowed origins configurable
 socketio = SocketIO(app, logger=True, cors_allowed_origins="http://localhost:3000")
 
 @socketio.on('connect')
 def connected():
     print('on connect event')
 
-@socketio.on_error_default
-def default_error_handler(e):
-    print(request.event["message"]) # "my error event"
-    print(request.event["args"])  
+@app.route("/run", methods=['POST'])
+@cross_origin()
+def run_executable():
+    content_type = request.headers.get("Content-Type")
+    if content_type != "application/json":
+        return "Specified content type is not supported", 400 
+    request_body = request.get_json()
 
-#def run_executable():
-#    content_type = request.headers.get("Content-Type")
-#    if content_type != "application/json":
-#        return "Specified content type is not supported", 400 
-#    request_body = request.get_json()
-#
-#    if request_body is None:
-#        return "Body is not specified", 400
-#
-#    executable_path = request_body["path"]
-#
-#    return executable_path
+    if request_body is None:
+        return "Body is not specified", 400
+
+    executable_path = request_body["path"]
+
+    return executable_path
 
 if __name__ == "__main__":
     argument_parser = argparse.ArgumentParser(
