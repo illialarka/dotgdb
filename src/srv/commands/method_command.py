76cf41c4ad8765d8659e728601689fabc3d909e1
Illia Larka <larkailya@gmail.com>
2022-11-15 19:44:21 +0100

    Add returning method signature to method command

--- src/srv/commands/method_command.py
+++ src/srv/commands/method_command.py
@@ -1,5 +1,10 @@
+from collections import namedtuple
 import commands.command as cmd
 import argparse
+
+MethodSignature = namedtuple(
+    "MethodSignature",
+    ["return_type", "method_name", "parameters"])
 
 class MethodCommand(cmd.Command):
 
@@ -11,8 +16,14 @@ class MethodCommand(cmd.Command):
         self._argument_parser = argparse.ArgumentParser(prog='method') 
 
         self._argument_parser.add_argument('--method-id', help='specifies methods identifier', required=True, type=int)
-        self._argument_parser.add_argument('subcommand', help='specifies subcomand', choices=['get', 'locations'], default='get')
-    
+        self._argument_parser.add_argument(
+            'subcommand',
+            help='specifies subcomand',
+            choices=['get', 'locations', 'body', 'signature'],
+            default='get',
+            type=str,
+            nargs='?')
+ 
     def execute(self, agent, args):
         arguments = None
         try:
@@ -33,9 +44,29 @@ class MethodCommand(cmd.Command):
             for location in self._get_method_locations(agent, arguments.method_id):
                 print(location)
             return
+        
+        if arguments.subcommand == 'body':
+            print(self._get_method_body(agent, arguments.method_id))
+            return
+        
+        if arguments.subcommand == 'signature':
+            print(self._get_method_signature(agent, arguments.method_id))
+            return
 
     def _get_method(self, agent, method_id):
         return agent.vm.get_method(method_id)
 
     def _get_method_locations(self, agent, method_id):
         return agent.vm.get_method(method_id).get_code_locations()
+    
+    def _get_method_body(self, agent, method_id):
+        return agent.vm.get_method(method_id).get_body()
+    
+    def _get_method_signature(self, agent, method_id):
+        method = agent.vm.get_method(method_id)
+
+        return_type = method.get_return_type()
+        method_name = method.get_name()
+        parameters = method.get_params()
+
+        return MethodSignature(return_type, method_name, parameters)
--- src/srv/commands/selector.py
+++ src/srv/commands/selector.py
@@ -2,7 +2,6 @@ import commands.appdomain_command as appdomain
 import commands.exit_command as exit_cmd
 import commands.supported_commands as supported_cmd
 import commands.threads_command as threads_cmd
-import commands.thread_frames_command as thread_frames_cmd 
 import commands.assemblies_command as get_assemblies_cmd
 import commands.assembly_command as get_assembly_cmd
 import commands.type_command as type_cmd
@@ -13,7 +12,6 @@ supported_commands = set([
     exit_cmd.ExitCommand(),
     supported_cmd.SupportedCommands(),
     threads_cmd.ThreadsCommand(),
-    thread_frames_cmd.ThreadFramesCommand(),
     get_assemblies_cmd.AssembliesCommand(),
     get_assembly_cmd.AssemblyCommand(),
     type_cmd.TypeCommand(),
