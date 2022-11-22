527e0a905f50102c8e221bbfeeb40e57e557514e
Illia Larka <larkailya@gmail.com>
2022-11-22 21:11:46 +0100

    Implement setting breakpoint

--- src/srv/commands/breakpoint_command.py
+++ src/srv/commands/breakpoint_command.py
@@ -1,61 +1,91 @@
+from collections import namedtuple
 import commands.command as cmd
 import event_modifiers
 import constants
 import argparse
 
+LocationParsed = namedtuple(
+    'LocationParsed',
+    ['type_name', 'method_name', 'line_number'])
+
 class BreakpointCommand(cmd.Command):
 
     def __init__(self):
-        self.aliases = ['breakpoint', 'bt']
+        self.aliases = ['breakpoint', 'break', 'bt']
         self.description = 'Manages breakpoints'
         self.help = 'Usage: breakpoint <action>'
 
         self._argument_parser = argparse.ArgumentParser() 
-
         self._argument_parser.add_argument(
-            'subcommand',
-            choices=['set', 'list', 'remove', 'unsetall'],
-            default='get',
-            type=str,
-            nargs='?')
-
-        self._argument_parser.add_argument('--method-id', help='specifies breakpoint method indentifier', type=int)
-        self._argument_parser.add_argument('--il-offset', help='specifies breakpoint method offset', type=int)
-
+            'location',
+            help=f'Sets breakpoint at specified location. Location should follow patter - <namespace.type:method:line_number>',
+            type=str)
+ 
     def execute(self, agent, args=None):
         arguments = None
         try:
             arguments = self._argument_parser.parse_args(args)
         except:
             return
+       
+        location = self._parse_breakpoint_location(arguments.location)
 
-        if arguments is None:
+        if location is None:
+            print('Unable to understand location you provided. Please use [-help] to get help information.')
             return
 
-        if arguments.subcommand == 'list':
-            pass
+        self._set_breakpoints(agent, location.type_name, location.method_name, location.line_number)
 
-        if arguments.subcommand == 'set':
-            if arguments.method_id is None or arguments.il_offset is None:
-                print("Method id and IL offset should be specified")
-                return
+    def _set_breakpoints(self, agent, type_name, method_name, line_number):
+        assemblies = agent.vm.get_root_appdomain().get_assemblies()
+        method_break_on = None
+        event_request = None
 
-            event_request = self._set_breakpoints(
-                agent,
-                event_modifiers.LocationModifier(arguments.method_id, arguments.il_offset))
-            print("Breakpoint is set. Event request {0}".format(event_request))
+        for assembly in assemblies:
+            type = assembly.get_type_by_name(type_name) 
+            if type is None:
+                continue
 
-        if arguments.subcommand == 'remove':
-            pass
+            method = type.get_method_by_name(method_name)
+            if method is None:
+                continue
 
-        if arguments.subcommand == 'unsetall':
-            pass
+            method_break_on = method
 
-    def _set_breakpoints(self, agent, location):
-        return agent.enable_event(
-            constants.EVENT_KIND_BREAKPOINT,
-            constants.SUSPEND_POLICY_ALL,
-            location)
+        if method_break_on is None:
+            print(f'Location to breakpoint was not found. Make sure you are at right position.')
+            return
+        
+        code_locations = method_break_on.get_code_locations()
+
+        for code_location in code_locations:
+            if code_location.line_number == line_number:
+                breakpoint_location = event_modifiers.LocationModifier(method_break_on.id, code_location.il_offset) 
+
+                event_request = agent.enable_event(
+                    constants.EVENT_KIND_BREAKPOINT,
+                    constants.SUSPEND_POLICY_ALL,
+                    breakpoint_location)
+                break 
+        
+        il_offset = code_location.il_offset
+        method_file = method_break_on.get_source_filename() 
+        breakpoint_id = event_request.request_id
+        print(f'Breakpoint {breakpoint_id} has been set at 0x{il_offset:02X}: {method_file}, line {line_number}.')
 
     def _unset_all_breakpoints(self, agent):
         agent.disable_all_breakpoints()
+    
+    def _list_breakpoints(self, agent):
+        breakpoints = agent.breakpoints
+        print('Breakpoints list\n')
+        for index in range(0, len(breakpoints)):
+            breakpoint = breakpoints[index] 
+            print(f'#{index} breakpoint at {breakpoint}')
+    
+    def _parse_breakpoint_location(self, location):
+        parts = location.split(':')
+        return LocationParsed(
+            parts[0],
+            parts[1],
+            int(parts[2]) if parts[2].isdigit() else None) if len(parts) == 3 else None
