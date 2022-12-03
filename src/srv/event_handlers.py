15f701b3c17597d3f66c5c6e3b0d53888dddcc8c
Illia Larka <larkailya@gmail.com>
2022-12-03 17:57:47 +0100

    Refactor event handlers

--- src/srv/event_handlers.py
+++ src/srv/event_handlers.py
@@ -1,20 +1,23 @@
 import constants
-from cli_context import CliContextService 
+from cli_context import CliContextService
+
+cli_context_service = CliContextService()
+
 
 def on_vm_start(event):
-    print('Event <(kind = {0}), (request_id = {1}), (thread_id = {2})>'
-        .format(constants.EVENT_FRIENDLY_NAME[event.event_kind], event.request_id, event.thread_id))
+    write_event_basic_info(event)
+
 
 def on_breakpoint(event):
-    print('Event <(kind = {0}), (request_id = {1}), (thread_id = {2})>'
-        .format(constants.EVENT_FRIENDLY_NAME[event.event_kind], event.request_id, event.thread_id))
-    context_service = CliContextService() 
-    context_service.break_on(event)
-
-def on_step(event):
-    print('Event <(kind = {0}), (request_id = {1}), (thread_id = {2})>'
-        .format(constants.EVENT_FRIENDLY_NAME[event.event_kind], event.request_id, event.thread_id))
-    print('Step over')
-    cli_context_service = CliContextService() 
+    write_event_basic_info(event)
     cli_context_service.break_on(event)
 
+
+def on_step(event):
+    write_event_basic_info(event)
+    cli_context_service.break_on(event)
+
+
+def write_event_basic_info(event):
+    print('Event {0} received with request identifier {1} at the thread {2}.' .format(
+        constants.EVENT_FRIENDLY_NAME[event.event_kind], event.request_id, event.thread_id))
