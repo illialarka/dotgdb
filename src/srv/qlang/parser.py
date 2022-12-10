d81f594a98f8bea2a53aae390e7c81a22c842815
Illia Larka <larkailya@gmail.com>
2022-12-10 20:01:41 +0100

    Implement different queries example

--- src/srv/qlang/parser.py
+++ src/srv/qlang/parser.py
@@ -1,15 +1,23 @@
 from lark import Lark
+import argparse
 import utils 
 
-grammar_path = './grammar.lark' 
+grammar_path = './grammar.ebnf' 
 
 # Define the ENBF grammar using the Lark parser
 enbf_grammar = open(grammar_path).read() 
 
-# Parse the query using the Lark parser
-query = "from table where id = 2 and name = divan and name = \"sofa\" select field"
+queries = {
+    'plain': "from table where id = 2 and name = divan and name = \"sofa\" or name = \"somevalue\" and id = 3 select field, another, andonemore"
+}
 
+argument_parser = argparse.ArgumentParser()
+argument_parser.add_argument("query", choices=['plain'])
+
+arguments = argument_parser.parse_args() 
+
+# Parse the query using the Lark parser
 parser = Lark(grammar=enbf_grammar, start='start', ambiguity='explicit')  
-parsed = parser.parse(query)
+parsed = parser.parse(queries[arguments.query])
 
 utils.display_tree(parsed)
