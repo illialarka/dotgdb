688949c76ad8af174827f56a0d193e443d6090ee
Illia Larka <larkailya@gmail.com>
2022-12-10 20:04:04 +0100

    Implement function in select clause

--- src/srv/qlang/parser.py
+++ src/srv/qlang/parser.py
@@ -8,11 +8,12 @@ grammar_path = './grammar.ebnf'
 enbf_grammar = open(grammar_path).read() 
 
 queries = {
-    'plain': "from table where id = 2 and name = divan and name = \"sofa\" or name = \"somevalue\" and id = 3 select field, another, andonemore"
+    'plain': "from table where id = 2 and name = divan and name = \"sofa\" or name = \"somevalue\" and id = 3 select field, another, andonemore",
+    'average': 'from table select avg(memory)'
 }
 
 argument_parser = argparse.ArgumentParser()
-argument_parser.add_argument("query", choices=['plain'])
+argument_parser.add_argument("query", choices=['plain', 'average'])
 
 arguments = argument_parser.parse_args() 
 
