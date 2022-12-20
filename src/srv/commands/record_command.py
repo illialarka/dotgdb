c3a632f8e9ef86a5ea6f08f1f96ff4c64b98b7a2
Illia Larka <larkailya@gmail.com>
2022-12-20 18:19:55 +0100

    Add buletpoints

--- src/srv/commands/record_command.py
+++ src/srv/commands/record_command.py
@@ -14,19 +14,20 @@ class RecordCommand(Command):
         # and will query event descriptor query and 
         # save data to some temporary store
         # 
-        # Thinking about storage, in memory is good for short lived 
+        # Thinking about storage.
+        # [ ] In memory is good for short lived 
         # data, but there is a risk to get OutOfMem error.
         # So, probably, it would be great to have some output file 
         # with streaming data (applying batching). 
         #
         # Thinking about visualiation.
         # 
-        # 1. Use built-in functions (code) to display a charts/graphs etc.
-        # 2. Just save to file, for future analyzation (CSV,JSON) 
+        # 1.[ ] Use built-in functions (code) to display a charts/graphs etc.
+        # 2.[ ] Just save to file, for future analyzation (CSV,JSON) 
         # 
         # Thinking about stopping of recording.
         # 
-        # 1. Infinit loops - restrict with iteration/time of execution
-        # 2. Just to complete the programm 
-        # 3. By user interruption
+        # 1.[ ] Infinit loops - restrict with iteration/time of execution
+        # 2.[ ]  Just to complete the programm 
+        # 3.[ ] By user interruption
         #  
