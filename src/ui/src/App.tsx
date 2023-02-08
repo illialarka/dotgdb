efbfd4194e3cf7ba343d4c6dc9ed40c6b38ee2f4
Illia Larka <larkailya@gmail.com>
2023-02-08 20:38:45 +0100

    Add UI part of dotgdb

---
+++ src/ui/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Loader from './components/loader';
+
+function App() {
+  return (
+    <div>
+      <div className="w-1/3 md:w-2/3">
+        <Loader></Loader>
+      </div>
+    </div>
+  );
+}
+
+export default App;
